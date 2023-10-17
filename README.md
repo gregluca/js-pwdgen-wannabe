8932f2a7e05debaaaadafe09d6b480454487d39a
Luca Gregori <luca.gregori98@gmail.com>
2023-10-17 16:56:51 +0200

    Add:  scaffolding html - css - js - Create README

---
+++ README.md
@@ -0,0 +1,5 @@
+# Password Generetor 
+
+1. Calcoliamo l'anno di nascita dell'utente 
+2. Impostiamo la domanda del cognome 
+3. Impostiamo domanda del colore preferito.
---
+++ js/script.js
@@ -0,0 +1,7 @@
+const currentYear = 2023; 
+
+const userAge = parseInt (prompt("Ciao Utente! Quanti anni hai?"));
+console.log(userAge, typeof userAge);
+
+const userSurname = parseInt (prompt("Ciao Utente! Scrivi il tuo cognome?"));
+console.log(userSurname, typeof userSurname);
