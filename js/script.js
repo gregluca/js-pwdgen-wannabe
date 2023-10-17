f949e9f8d78ba11b1f75ef13a7ed57b2410136eb
Luca Gregori <luca.gregori98@gmail.com>
2023-10-17 17:17:07 +0200

    Add: js

--- js/script.js
+++ js/script.js
@@ -1,7 +1,15 @@
 const currentYear = 2023; 
 
-const userAge = parseInt (prompt("Ciao Utente! Quanti anni hai?"));
-console.log(userAge, typeof userAge);
+const userName = prompt("Ciao Utente! Come ti chiami?");
+console.log(userName);
 
-const userSurname = parseInt (prompt("Ciao Utente! Scrivi il tuo cognome?"));
-console.log(userSurname, typeof userSurname);
+const userSurname = prompt("Ciao Utente! Scrivi il tuo cognome?");
+console.log(userSurname);
+
+const userColor = prompt("Ciao Utente! Qual'è il tuo colore preferito?");
+console.log(userColor);
+
+const resultMessage = (`${userName} ${userSurname} ${userColor} ${21} `);
+console.log(resultMessage);
+
+document.getElementById ("result") .innerHTML = resultMessage;
